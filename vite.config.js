9905364ab45298726e32cedfb97f39111aad65e7
Luke Carl Thompson <lukecarlthompson@gmail.com>
2022-05-30 13:46:49 +0800

    adjust vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    outDir: 'docs',
-  },
-  base: '/Pixijs-Space-Shooter/',
+export default defineConfig(({ command, mode }) => {
+  if (mode === 'production') {
+    return {
+      build: {
+        outDir: 'docs',
+      },
+      base: '/Pixijs-Space-Shooter/',
+    };
+  } else {
+    return {};
+  }
 });
